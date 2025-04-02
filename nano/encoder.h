2223fda6ef9f8d08f3c6338b5908769d6085a3a8
Manjit Baishya <manjitbaishya01@gmail.com>
2025-04-03 00:36:39 +0530

    modified tinyml files

--- nano/encoder.h
+++ nano/encoder.h
@@ -4,6 +4,7 @@
 #define NUM_CLASSES 5
 
 const char* class_labels[NUM_CLASSES] = {"BRCA", "COAD", "KIRC", "LUAD", "PRAD"};
+
 const int class_values[NUM_CLASSES] = {0, 1, 2, 3, 4};
 
 #endif // LABEL_ENCODER_H
